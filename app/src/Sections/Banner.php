8169001345bd52555f594cc7d6e5a92af43cf647
Keith Loise <k8t.loise16@gmail.com>
2021-03-24 12:17:04 +1300

    Added new Sections

---
+++ app/src/Sections/Banner.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace {
+
+    use SilverStripe\AssetAdmin\Forms\UploadField;
+    use SilverStripe\Assets\Image;
+    use SilverStripe\Forms\FieldList;
+    use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+
+    class Banner extends Section
+    {
+        private static $singular_name = 'Banner';
+
+        private static $db = [
+            'Content' => 'HTMLText',
+        ];
+
+        private static $has_one = [
+            'Image' => Image::class
+        ];
+
+        private static $owns = [
+            'Image'
+        ];
+
+        public function getSectionCMSFields(FieldList $fields)
+        {
+            $fields->addFieldToTab('Root.Main', UploadField::create('Image', 'Banner image')
+                ->setFolderName('Banner/Images'));
+            $fields->addFieldToTab('Root.Main', HTMLEditorField::create('Content'));
+        }
+    }
+}
