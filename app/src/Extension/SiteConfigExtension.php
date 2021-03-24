b24fac3fc1e0b68c10123aeaf00e6f7725b54523
Keith L <k8t_loise16@yahoo.com>
2021-03-24 18:29:54 +1300

    Update SiteConfigExtension.php

--- app/src/Extension/SiteConfigExtension.php
+++ app/src/Extension/SiteConfigExtension.php
@@ -5,6 +5,8 @@ namespace {
     use SilverStripe\AssetAdmin\Forms\UploadField;
     use SilverStripe\Assets\Image;
     use SilverStripe\Forms\FieldList;
+    use SilverStripe\Forms\GridField\GridField;
+    use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
     use SilverStripe\ORM\DataExtension;
 
     class SiteConfigExtension extends DataExtension
@@ -21,6 +23,20 @@ namespace {
         {
             $fields->addFieldToTab('Root.Main', UploadField::create('SiteLogo')
                 ->setFolderName('SiteLogo'));
+
+            /*
+             * Section Width
+             */
+            $configWidth = GridFieldConfig_RecordEditor::create('999');
+            $editorWidth = GridField::create('SectionWidth', 'Width', Width::get(), $configWidth);
+            $fields->addFieldToTab('Root.SectionSettings', $editorWidth);
+
+            /*
+             * Animation
+             */
+            $configAnimation = GridFieldConfig_RecordEditor::create('999');
+            $editorAnimation = GridField::create('Animations', 'Animation', Animations::get(), $configAnimation);
+            $fields->addFieldToTab('Root.SectionSettings', $editorAnimation);
         }
     }
 }
