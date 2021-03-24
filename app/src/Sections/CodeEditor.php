05ed08040fabb1cf561390443074839b0b25d9f5
Keith L <k8t_loise16@yahoo.com>
2021-03-24 18:29:26 +1300

    Added CodeEditor.php

---
+++ app/src/Sections/CodeEditor.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace {
+
+    use SilverStripe\Forms\FieldList;
+    use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+    use SwiftDevLabs\CodeEditorField\Forms\CodeEditorField;
+
+    class CodeEditor extends Section
+    {
+        private static $singular_name = 'Code Editor';
+
+        private static $db = [
+            'CodeEditorText' => 'HTMLText',
+        ];
+
+        public function getSectionCMSFields(FieldList $fields)
+        {
+            $fields->addFieldToTab('Root.Main', CodeEditorField::create('CodeEditorText', 'Code editor'));
+        }
+    }
+}
